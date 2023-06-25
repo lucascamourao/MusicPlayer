d4f547dc9d1ef275e13ce8523c5b39e4d4b3614e
GuilhermeGalvao1 <guigalvao@alu.ufc.br>
2023-06-25 17:47:13 -0300

    Primeiro commit

---
+++ src/application/MusicPlayer.java
@@ -0,0 +1,33 @@
+package application;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+
+public class MusicPlayer extends Application {
+	@Override
+	public void start(Stage stage) throws Exception {
+
+		Parent root = FXMLLoader.load(getClass().getResource("/view/MainView.fxml"));
+		Scene scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+
+		stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+			@Override
+			public void handle(WindowEvent arg0) {
+				Platform.exit();
+				System.exit(0);
+			}
+		});
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
